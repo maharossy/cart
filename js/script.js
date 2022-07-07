77865351f3a96aeff000358781d042a0a63e42ef
Vladyslav Marynets <smarynec@gmail.com>
2022-07-07 22:18:02 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ window.addEventListener('DOMContentLoaded', function() {
           buttons = document.querySelectorAll('button'),
           openBtn = document.querySelector('.open');
 
+    createCart();      
+
     function createCart() {
         let cart = document.createElement('div'),
-            field = document.createComment('div'),
-            heading = document.createComment('h2'),
+            field = document.createElement('div'),
+            heading = document.createElement('h2'),
             closeBtn = document.createElement('button');
 
         cart.classList.add('cart');
@@ -17,7 +19,39 @@ window.addEventListener('DOMContentLoaded', function() {
         closeBtn.textContent = 'Закрыть';
 
         document.body.appendChild(cart);
+        cart.appendChild(heading);
+        cart.appendChild(field);
+        cart.appendChild(closeBtn);
     }      
+
+        const field = document.querySelector('.cart-field'),
+              cart = document.querySelector('.cart'),
+              close = document.querySelector('.close');
+
+    function openCart() {
+        cart.style.display = 'block';
+    }
+
+    function closeCart() {
+        cart.style.display = 'none';
+    }
+
+    openBtn.addEventListener('click', openCart);
+
+    close.addEventListener('click', closeCart);
+
+    buttons.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            const item = product[i].cloneNode(true),
+                  btn = item.querySelector('button');
+
+            btn.remove();
+            field.appendChild(item);
+            product[i].remove();
+        });
+    });
 });
+
+
 
 
